35cfb2fc3c93cc41cebd218d6e06e54fd7a597f2
francis <francis@sinonet.ph>
2017-10-12 15:02:00 +0800

    initial set up

---
+++ src/main/java/com/arbalest/mycrew/MyCrewApplication.java
@@ -0,0 +1,12 @@
+package com.arbalest.mycrew;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyCrewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyCrewApplication.class, args);
+	}
+}
